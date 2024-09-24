22b89002d65b3e6ec290cc4b561e389c377d081e
Angel Pearl Narvarte <jinjukim1206@gmail.com>
2024-09-25 00:58:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R. id.Roll)
+        val imageView = findViewById<ImageView>(R. id.dice1)
+
+    button.setOnClickListener {
+        val diceRoll = (1 ..6).random()
+
+        imageView.setImageResource(when(diceRoll){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> throw IllegalArgumentException("Not valid roll")
+
+        })
+    }
+    }
+}
